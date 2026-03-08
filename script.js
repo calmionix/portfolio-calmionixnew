7ea2ebc0b27ca0d47e313567960c946f48fd8125
calmionix <calmionix@gmail.com>
2026-03-08 12:40:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-// LOADING SCREEN
-window.addEventListener("load",function(){
+
+// LOADING
+
+window.addEventListener("load",()=>{
 
 setTimeout(()=>{
+
 document.getElementById("loader").style.opacity="0";
 
 setTimeout(()=>{
-document.getElementById("loader").style.display="none";
-},500);
 
-},2000);
+document.getElementById("loader").style.display="none";
+
+},600);
+
+},1800);
 
 });
 
+
 // MENU
+
 function toggleMenu(){
 
 document.getElementById("menu").classList.toggle("active");
 
 }
 
-// DARK / LIGHT MODE
+
+// DARK MODE
+
 function toggleMode(){
 
 document.body.classList.toggle("light");
