c94c4183502b6fd106bdaee43d809c3690fea7aa
calmionix <calmionix@gmail.com>
2026-03-08 12:48:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,48 @@ function toggleMode(){
 document.body.classList.toggle("light");
 
 }
+
+let selectedPackage = "";
+let selectedType = "";
+
+function openPopup(type){
+
+selectedType = type;
+
+document.getElementById("popupTitle").innerText =
+"Pesan " + type;
+
+document.getElementById("ratePopup").style.display="flex";
+
+}
+
+function selectOption(el){
+
+document.querySelectorAll(".popup-option")
+.forEach(o=>o.style.background="#132c47");
+
+el.style.background="#5b5ce2";
+
+selectedPackage = el.innerText;
+
+}
+
+function orderWA(){
+
+if(!selectedPackage){
+alert("Pilih paket dulu!");
+return;
+}
+
+const message =
+"Halo Calmionix, saya ingin pesan " +
+selectedType +
+" - " +
+selectedPackage;
+
+window.open(
+"https://wa.me/6282130570915?text=" +
+encodeURIComponent(message)
+);
+
+}
